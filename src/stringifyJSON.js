57b477ef5f53f1b475c3e450aa6325b487380d49
walfly <walker.d.flynn@gmail.com>
2013-04-16 14:29:12 -0500

    not recursive on every element

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,26 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+	var string = '';
+	var stringifier = function (obje) {    
+	if (obje.length){ 
+	    string += obje.shift() + ',';  
+	    if (obje.length){
+	        stringifier(obj);
+	    }
+	} else {
+	    var key2 = [];
+	    for(key in obje){
+	        key2.push(key);
+	    }
+	    var key2I = key2.shift();
+	    string += key2I + ':' + obje[key2I] + ',';
+	    delete obje[key2I];
+	    if (key2.length){
+	        stringifier(obje);
+	    }
+	}
+	return string;
+	};
+	return stringifier(obj);
 };
