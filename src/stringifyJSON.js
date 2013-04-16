b094fae440ab28765fa157f4caf83c7a8a3b4fa3
walfly <walker.d.flynn@gmail.com>
2013-04-16 16:09:44 -0500

    lots of testing

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,25 +4,35 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
 	var string = '';
-	var stringifier = function (obje) {    
-	if (obje.length){ 
-	    string += obje.shift() + ',';  
-	    if (obje.length){
-	        stringifier(obj);
-	    }
-	} else {
-	    var key2 = [];
-	    for(key in obje){
-	        key2.push(key);
-	    }
-	    var key2I = key2.shift();
-	    string += key2I + ':' + obje[key2I] + ',';
-	    delete obje[key2I];
-	    if (key2.length){
-	        stringifier(obje);
-	    }
-	}
-	return string;
-	};
-	return stringifier(obj);
+    var stringifier = function (obj1){
+        if (obj1.length){
+            var thisIndex = obj1.shift();
+            if (typeof thisIndex !== "number" && typeof thisIndex !== "string" && typeof thisIndex !== "boolean" && thisIndex !== null && !thisIndex.length){
+                stringifier(thisIndex); 
+            } else {
+                string += thisIndex + ',';
+                if(obj1.length){
+                    stringifier(obj1);  
+                }
+            }
+        } else {
+            var keys = [];
+            for (key in obj1){
+                keys.push(key);  
+            }
+            var key1 = keys.shift();
+            var thisObj = obj1[key1];
+            if (typeof thisObj !== "number" && typeof thisObj !== "string" && typeof thisObj !== "boolean" && thisObj !== null && !thisObj.length){
+                  stingifier(thisObj);
+            } else {
+                string += key1 + ':' + thisObj + ','; 
+                delete obj1[key1];
+                if (keys.length){
+                    stringifier(obj1);  
+                }
+            }
+        }
+        return string;
+    };
+    return stringifier(obj);
 };
